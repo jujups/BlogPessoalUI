5799d87091566f63882dfaebe8041b4c526849a5
Giullia Maria <giulllia_mrs@yahoo.com>
2021-07-05 13:04:10 -0300

    rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastro.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: "", redirectTo: 'entrar' , pathMatch: 'full'},
+  
+  {path: 'entar', component: EntrarComponent},
+  {path: 'cadastrar' , component: CadastrarComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
