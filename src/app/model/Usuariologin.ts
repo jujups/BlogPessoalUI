fafb84921bac1158704b32fdd2e99ee6de09e03d
Giullia Maria <giulllia_mrs@yahoo.com>
2021-07-11 22:40:43 -0300

    rotas de entar e cadastrar criadas

---
+++ src/app/model/Usuariologin.ts
@@ -0,0 +1,9 @@
+export class Usuariologin{
+    public id: number
+    public nome: string
+    public usuario: string
+    public senha: string 
+    public token: string
+    public foto: string
+    public tipo: string
+}
